aaeb2a5b35c5118cc6f4ec1dfd48faef5d229e58
labhiCS <lamichhaneabhi10@gmail.com>
2024-09-01 08:30:22 +0545

    get()_set()

---
+++ Student_1.java
@@ -0,0 +1,33 @@
+//import java.security.PublicKey;
+//import java.sql.PseudoColumnUsage;
+
+public class Student_1 {
+    private int age;
+    private String name;
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public  void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public String getName(){
+        return this.name;       
+    }
+
+    public static void main(String[] args) {
+        Student_1 student_1 = new Student_1();
+        student_1.setAge(19);
+        student_1.setName("Ram");
+
+        System.out.println(student_1.getName());
+        System.out.println(student_1.getAge());
+        
+    }   
+}
