3d383429d952263494473d11b3d0172f6286d007
labhiCS <lamichhaneabhi10@gmail.com>
2024-09-01 08:28:27 +0545

    Student

--- Student.java
+++ Student.java
@@ -1,11 +1,12 @@
 public class Student {
     String name;
     int rollno;
+    int age;
     
     public void displayDetails(){
         System.out.println("Name: " + name);
         System.out.println("rollno: " + rollno);
-        
+        System.out.println("age: " + age);
 
     }
     
@@ -14,12 +15,12 @@ public class Student {
         Student student1 = new Student();
         student1.name = "Abhishek";
         student1.rollno = 2;
-        
+        student1.age = 21;
 
         Student student2 = new Student();
         student2.name = "siddhi";
         student2.rollno = 24;
-        
+        student2.age = 18;
 
         System.out.println("Detail of student: ");
         student1.displayDetails();
