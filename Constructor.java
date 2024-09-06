678e7b2282dcb45182e06cbe3d50e0794a8220b8
labhiCS <lamichhaneabhi10@gmail.com>
2024-09-06 10:49:56 +0545

    Include all Constructor.

---
+++ Constructor.java
@@ -0,0 +1,33 @@
+public class Constructor {
+    int num, den;
+    public Constructor(){
+        this.num = 55;
+        this.den = 0;
+    }
+
+    public Constructor(int a, int b){
+        this.num = a;
+        this.den = b; 
+    }
+
+    public Constructor (Constructor n){
+        this.num = n.num;
+        this.den = n.den;
+    }
+
+    public void display(){
+        System.out.println("The number is: " + num);
+        System.out.println("Another number is: "+ den);
+    }
+
+    public static void main (String []args){
+        Constructor a = new Constructor();
+        Constructor a1 = new Constructor(20, 30);
+        Constructor a2 = new Constructor(a);
+
+        a.display();
+        a1.display(); 
+        a2.display();
+    }
+    
+}
